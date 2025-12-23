3e9d8fbadf1a62951d0f8f40583746af575bd3ee
Mathias Schindler <3246297+MathiasSchindler@users.noreply.github.com>
2025-12-23 10:27:43 +0100

    compat

--- core/mc_libc_compat.c
+++ core/mc_libc_compat.c
@@ -1,14 +1,25 @@
 #include "mc.h"
 
-void *(memcpy)(void *dst, const void *src, mc_usize n) {
+// Some libcs expose these as macros. Ensure we're defining real functions.
+#ifdef memcpy
+#undef memcpy
+#endif
+#ifdef memmove
+#undef memmove
+#endif
+#ifdef memset
+#undef memset
+#endif
+
+void *memcpy(void *dst, const void *src, mc_usize n) {
 	return mc_memcpy(dst, src, n);
 }
 
-void *(memmove)(void *dst, const void *src, mc_usize n) {
+void *memmove(void *dst, const void *src, mc_usize n) {
 	return mc_memmove(dst, src, n);
 }
 
-void *(memset)(void *dst, int c, mc_usize n) {
+void *memset(void *dst, int c, mc_usize n) {
 	return mc_memset(dst, c, n);
 }
 
